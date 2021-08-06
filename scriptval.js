873a9a2c90e9eab97bbf80abe3a3f35cb6e8e249
Sarang VS <sarang6180@gmail.com>
2021-08-06 09:24:10 +0530

    dbms added

--- scriptval.js
+++ scriptval.js
@@ -17,4 +17,23 @@ $(document).ready(function(){
       
 
     })
+    $("#contact-form").submit((e)=>{
+        e.preventDefault()
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbwvWelKFl3L0TpURczs9j3mrSKNPRFlRSEOmi7N98b_r_ZtXiRl4e5s9cXDqBHKIwew/exec",
+            data:$("#contact-form").serialize(),
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+
+            }
+        })
+    })
 })
+
+       
