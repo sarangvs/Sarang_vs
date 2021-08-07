11e1412e7c373e5d37390a5ab889e9ac835ff4e2
Sarang VS <sarang6180@gmail.com>
2021-08-07 11:28:55 +0530

    Validation updated

--- scriptval.js
+++ scriptval.js
@@ -1,75 +1,88 @@
 $(document).ready(function(){
-    $("#contact-form").keyup(function(){
-      if (this.value != this.value.replace(/[^a-zA-Z. .\.]/g, '')){
-        this.value=this.value.replace(/[^a-zA-Z\.]/g, '');
-        if(name.value ==='' || name.value ==null){
-          message.push("Minimum 3 character is requierd");
-        }
-      }
-    }) 
-    $("#inputnumber").keyup(function(){
-      if (this.value != this.value.replace(/[^0-9.\.]/g, '')){
-        this.value=this.value.replace(/[^0-9\.]/g, '');
-        if(mobile.value.length >=10){
-          message.push("Please enter a valid mobile number")
-        }
-
-      }
-    })
-    $("#feedback").keyup(function(){
-        if (this.value != this.value.replace(/[^a-zA-Z. .\.]/g, '')){
-          this.value=this.value.replace(/[^a-zA-Z\.]/g, '');
-          if(name.value ==='' || name.value ==null){
-            message.push("Minimum 3 character is requierd");
-          }
-        }
-      }) 
-
-    $("#contact-form").validate({
-            rules:{
-                fname:{
-                required:true,
-                minlength:6
-              },
-              lname:{
-                required:true,
-                minlength:1
-              
-              },
-              email:{
-                required:true,
-                email:true
-              },
-              contactNum:{
-                required:true,
-                minlength:10,
-                maxlength:10
-              },
-              enquirY:{
-                  required:true,
-                  minlength:10
-              }
-              
-            },
-            submitHandler: function(form){
-            $.ajax({
-                url:"https://script.google.com/macros/s/AKfycbwvWelKFl3L0TpURczs9j3mrSKNPRFlRSEOmi7N98b_r_ZtXiRl4e5s9cXDqBHKIwew/exec",
-                data:$("#submit-form").serialize(),
-                method:"post",
-                success:function (response){
-                    alert("Form submitted successfully")
-                    window.location.reload()
-                    //window.location.href="https://google.com"
-                },
-                error:function (err){
-                    alert("Something Error")
-    
-                }
-            })
-          }
-        })
-
+  $.validator.addMethod("alpha", function (value, element) {
+      return this.optional(element) || value == value.match(/^[a-zA-Z\s]+$/);
   });
 
+  jQuery.validator.addMethod("noSpace", function (value, element) {
+      return value == '' || value.trim('').length >= 3;
+  }, "Minimum 3 character is required");
 
+  $.validator.addMethod("isEmail", function (value, element) {
+      return this.optional(element) || value == value.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i);
+  });
+  $("#contact-form").validate({
+      rules:{
+          fname:{
+              required:true,
+              alpha:true,
+              noSpace:true
+          },
+          lname:{
+              required:true,
+              alpha:true,
+              minlength:1
+          },
 
+          emailAdress:{
+              required:true,
+              email:true, 
+              isEmail:true
+          },
+          
+          contactNum:{
+              required:true,
+              digits:true,
+              minlength:10,
+              maxlength:10,
+              number:true
+  
+          },
+
+          enquirY:{
+              required:true,
+              minlength:10
+          }
+
+      },
+      messages:{
+          
+          fname:{
+              
+              alpha:"Please enter your valid name"
+          },
+          lname:{
+              alpha:"Please enter letters only"
+          },
+          contactNum:{
+      
+              minlength:"Enter a valid mobile number",
+              maxlength:"Enter a valid mobile number"
+          },
+          enquirY:{
+              minlength:"Please fill this field"
+          }
+      },
+      submitHandler:function(form){
+
+      $("#contact-form").submit((e)=>{
+          e.preventDefault()
+          $.ajax({
+              url:"https://script.google.com/macros/s/AKfycbwvWelKFl3L0TpURczs9j3mrSKNPRFlRSEOmi7N98b_r_ZtXiRl4e5s9cXDqBHKIwew/exec",
+              data:$("#contact-form").serialize(),
+              method:"post",
+              success:function (response){
+                  alert("Form submitted successfully")
+                  window.location.reload()
+                  //window.location.href="https://google.com"
+              },
+              error:function (err){
+                  alert("Something Error")
+  
+              }
+          })
+      })
+   }  
+
+  })
+
+})
