498528a6bd36528e5ff2577405faa9b36b3d8f68
Sarang VS <sarang6180@gmail.com>
2021-08-07 12:40:22 +0530

    toogle bug fixed

--- scriptval.js
+++ scriptval.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
   $.validator.addMethod("isEmail", function (value, element) {
       return this.optional(element) || value == value.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i);
   });
+  $('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
   $("#contact-form").validate({
       rules:{
           fname:{
